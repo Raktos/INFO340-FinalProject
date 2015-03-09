873b390fe9bca78da53600ded79ce2915dac34a6
Information School <ischoolit@uw.edu>
2015-03-08 17:38:59 -0700

    User insert working

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -20,3 +20,53 @@ AS
 INSERT INTO tblUSER_HEIGHT_WEIGHT(UserID, HWDate, UserHeight, UserWeight)
 VALUES(@UserID, @Date, @Height, @Weight
 GO
+
+--New User with initial Height Weight
+CREATE PROCEDURE uspInsertUser(
+	@FName varchar(60),
+	@LName varchar(60),
+	@Sex bit,
+	@DOB date,
+	@Email varchar(60),
+	@PassHash varchar(60),
+	@PassSalt int,
+	@Height decimal(5,2),
+	@Weight decimal(5,2)
+)
+AS
+DECLARE @ID int;
+BEGIN TRAN UserInsert;
+INSERT INTO tblUSER(
+	[UserFName],
+	[UserLName],
+	[UserSex],
+	[UserDOB],
+	[UserEmail],
+	[UserPassHash],
+	[UserPassSalt]
+)VALUES(
+	@Fname,
+	@LName,
+	@Sex,
+	@DOB,
+	@Email,
+	@PassHash,
+	@PassSalt
+);
+SELECT @ID = SCOPE_IDENTITY();
+INSERT INTO tblUSER_HEIGHT_WEIGHT(
+	[UserID],
+	[HWDate],
+	[UserHeight],
+	[UserWeight]
+)VALUES(
+	@ID,
+	GETDATE(),
+	@Height,
+	@Weight
+);
+COMMIT TRAN UserInsert;
+GO
+
+EXEC dbo.uspInsertUser 'John','Holt',0,'3-15-1987','email@email.com','878954',100,150.54,66.57;
+SELECT * FROM tblUSER;
