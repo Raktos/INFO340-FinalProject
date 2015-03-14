f5625c730411116efc7a9d585e79707419a2bf85
Information School <ischoolit@uw.edu>
2015-03-14 16:05:31 -0700

    UserHeightWeight SP

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -9,17 +9,16 @@
 USE WORKOUT_FACEBOOK;
 
 --Create Stored Procedure to Insert an existing users height and weight into tblUSER_HEIGHT_WEIGHT
---In Progress
- GO
- CREATE PROCEDURE uspUpdateUserHeightWeight
+CREATE PROCEDURE uspUpdateUserHeightWeight
 	@UserID int,
-	@Date date,
 	@Height decimal(5,2),
 	@Weight decimal(5,2)
 AS
 INSERT INTO tblUSER_HEIGHT_WEIGHT(UserID, HWDate, UserHeight, UserWeight)
-VALUES(@UserID, @Date, @Height, @Weight
+VALUES(@UserID, GETDATE(), @Height, @Weight)
 GO
+
+EXEC dbo.uspUpdateUserHeightWeight 1750, 72, 350;
 
 --New User with initial Height Weight
 CREATE PROCEDURE uspInsertUser(
@@ -70,3 +69,5 @@ GO
 
 EXEC dbo.uspInsertUser 'John','Holt',0,'3-15-1987','email@email.com','878954',100,150.54,66.57;
 SELECT * FROM tblUSER;
+
+--Create Stored Procedure to Insert new Group Activity
